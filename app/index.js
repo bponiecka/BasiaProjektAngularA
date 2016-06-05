dde09f8e182b6fdd921dd563acef905189f965a2
bponiecka <bponiecka@gmail.com>
2016-06-05 15:29:19 +0000

    user route

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
+//IMPORTUJE SOBIE FRAMEWORK BOOTSRRAP 
 import angular from 'angular';
+import uirouter from 'angular-ui-router';
+import routing from './config';
 
-const ngModule = angular.module('app',[]);
+import home from './features/directives'; 
+
+const ngModule = angular.module('app',[
+    uirouter,
+    home
+    ])
+    .config(routing);
